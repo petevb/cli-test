28f60459a6daa7b59c0592a387972cfc0fb1ccb0
Pete van Blerk <pete@van.blerk.net>
2016-11-18 15:21:41 +0000

    test tinkering

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,45 @@
 /* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 describe('AppComponent', () => {
-  beforeEach(() => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent
+
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-    });
+    }); //.compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
   });
 
-  it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
+  it('should create the app', () => {
     expect(app).toBeTruthy();
-  }));
+  });
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
+  it(`should have as title 'app works!'`, () => {
     expect(app.title).toEqual('app works!');
-  }));
+  });
 
-  it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
+  it('should render title in a h1 tag', () => {
     fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+    let nativeElement = fixture.debugElement.nativeElement;
+
+    // TODO pick one of these approaches:
+
+    expect(nativeElement.querySelector('h1').textContent).toContain('app works!');
+    
+    expect(nativeElement.textContent).toContain('app works!');
+
+    const h1 = nativeElement;
+    expect(h1.innerText).toMatch(/app works/i,
+      '<h1> should say something about "App Works"');
+  });
 });
