63adf211412ff41d18a4eb5388843cfa729f344b
Pete van Blerk <pete@van.blerk.net>
2016-11-26 12:02:21 +0000

    Update mock data for fullName

--- src/app/repo/repo-mock.ts
+++ src/app/repo/repo-mock.ts
@@ -2,10 +2,31 @@ import { Repo } from './repo';
 
 class MockData {
     static readonly REPOS: Repo[] = [
-        { id: "1", name: "test1", url: "http://test.github.io/", description: "lorem ipsum dolor sit amet" },
-        { id: "2", name: "test2", url: "http://test.github.io/", description: "lorem ipsum dolor sit amet" },
-        { id: "3", name: "test3", url: "http://test.github.io/", description: "lorem ipsum dolor sit amet" },
-        { id: "4", name: "test4", url: "http://test.github.io/", description: "lorem ipsum dolor sit amet" }
+        {
+            id: "1",
+            name: "test1",
+            url: "http://test.github.io/",
+            description: "lorem ipsum dolor sit amet",
+            fullName: "Test1/Repo1"
+        }, {
+            id: "2",
+            name: "test2",
+            url: "http://test.github.io/",
+            description: "lorem ipsum dolor sit amet",
+            fullName: "Test1/Repo2"
+        }, {
+            id: "3",
+            name: "test3",
+            url: "http://test.github.io/",
+            description: "lorem ipsum dolor sit amet",
+            fullName: "Test1/Repo3"
+        }, {
+            id: "4",
+            name: "test4",
+            url: "http://test.github.io/",
+            description: "lorem ipsum dolor sit amet",
+            fullName: "Test1/Repo4"
+        }
     ];
 
     public getData(): Promise<Repo[]> {
@@ -14,4 +35,3 @@ class MockData {
 }
 
 export const ReposMock = new MockData();
-//const RepoMock = mock.getData();
